54fdfa0059f8a8b8cdde05e4aae3fbbf803900e6
Kareem_DC7X <kareem741ahmed@gmail.com>
2023-09-14 21:25:44 +0300

    Add README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# ToolsK6sx 
+
+**__version -> 1.1**__ 
+
+## **require Tools :**
+
+`pip install -r requirements.txt`
+
+![img.png](resours/img.png)
+
+`python3 main.py --m `
+
+#### **Starting**Tools6sx **..........  21:21:37**
+
+![img_1.png](resours/img_1.png)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from Encrypt import CustomEncryption
 from lib.Color import *
 import os
 import argparse
+from time import sleep
 
 RunAllTools: bool = False
 TextEnc: bool = False
@@ -26,49 +28,57 @@ def Run() -> None:
         elif k6sx == "exit":
             exit(0)
 
+def main():
 
-parser = argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
 
-    prog=f"{Color.ANSI_BOLD}{Color.Blue}Tools6sx{Color.Reset}{Color.Reset}",
-    usage=f"{Color.ANSI_BOLD}{Color.Blue}Explain Using{Color.Reset}{Color.Reset}",
-    add_help=True
-)
-Group = parser.add_argument_group(f"{Color.banner}For Encryption :{Color.Reset}")
-parser.add_argument(
-    "--m",
-    f"--run",
-    help=f"{Color.ANSI_GREEN }--m or --run For Run  AllTools{Color.ANSI_1RESET}",
-    action='store_const',
-    const=True,
-    default=False,
-    dest="RunAllTools"
-)
-Group.add_argument(
-    "--tf",
-    "--text--file",
-    help=f"{Color.ANSI_YELLOW}--tf or --text--file For Encryption text and save text and salat in file.{Color.ANSI_1RESET}",
-    action='store_const',
-    const=True,
-    default=False,
-    dest="TextSave"
-)
-Group.add_argument(
-    "--t",
-    "--text",
-    help=f"{Color.ANSI_YELLOW}--t or --text For Encryption text only and return text Encryption and salat.{Color.ANSI_1RESET}",
-    action='store_const',
-    const=True,
-    default=False,
-    dest="TextEnc"
-)
+        prog=f"{Color.ANSI_BOLD}{Color.Blue}Tools6sx{Color.Reset}{Color.Reset}",
+        usage=f"{Color.ANSI_BOLD}{Color.Blue}Explain Using{Color.Reset}{Color.Reset}",
+        add_help=True
+    )
+    Group = parser.add_argument_group(f"{Color.banner}For Encryption :{Color.Reset}")
+    parser.add_argument(
+        "--m",
+        f"--run",
+        help=f"{Color.ANSI_GREEN }--m or --run For Run  AllTools{Color.ANSI_1RESET}",
+        action='store_const',
+        const=True,
+        default=False,
+        dest="RunAllTools"
+    )
+    Group.add_argument(
+        "--tf",
+        "--text--file",
+        help=f"{Color.ANSI_YELLOW}--tf or --text--file For Encryption text and save text and salat in file.{Color.ANSI_1RESET}",
+        action='store_const',
+        const=True,
+        default=False,
+        dest="TextSave"
+    )
+    Group.add_argument(
+        "--t",
+        "--text",
+        help=f"{Color.ANSI_YELLOW}--t or --text For Encryption text only and return text Encryption and salat.{Color.ANSI_1RESET}",
+        action='store_const',
+        const=True,
+        default=False,
+        dest="TextEnc"
+    )
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-if args.RunAllTools:
-    Run()
-elif args.TextEnc:
-    if args.TextSave:
-        print("Encrypt Text And Save ")
+    if args.RunAllTools:
+        Run()
+    elif args.TextEnc:
+        if args.TextSave:
+            print("Encrypt Text And Save ")
 
-    else:
-        print("Encrypt Text Not Save ")
+        else:
+            print("Encrypt Text Not Save ")
+
+
+if __name__ == "__main__":
+
+    print(f"\n{Color.ANSI_BOLD}{Color.banner}Starting Tools6sx ..........  {datetime.now().time()}{Color.Reset}{Color.ANSI_RESET}\n")
+    sleep(1)
+    main()
